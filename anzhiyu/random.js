2078f369392a6ab23b647f42fc026599a85dc6fc
hainoir <noirkl@outlook.com>
2025-04-14 20:00:47 +0800

    Site updated: 2025-04-14 20:00:47

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=[];function toRandomPost(){
+var posts=["2025/04/14/1.前缀和与差分/","2025/04/14/1-前缀和与差分/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
