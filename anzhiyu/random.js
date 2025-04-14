9f516295e25e679c19f92a4045b8c00574a7d2fb
hainoir <noirkl@outlook.com>
2025-04-14 19:37:56 +0800

    Site updated: 2025-04-14 19:37:56

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/14/hello-world/"];function toRandomPost(){
+var posts=["2025/04/14/1.前缀和与差分/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
